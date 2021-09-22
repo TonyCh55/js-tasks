ddcf74b6affc4daf18c6fa1c5c2a5ee3efeabcfe
TonyCh55 <chuprunant@gmail.com>
2021-09-22 18:19:04 +0300

    1.js refactored

--- 1.js
+++ 1.js
@@ -1,29 +1,15 @@
-const getUniqArr = (arr, func) => {
-  let uniq = [];
-
-  arr.forEach((item, idx, arr) => {
-    let current = func(item);
-    let next = func(arr[idx + 1]);
-
-    if (current !== next) {
-      uniq.push(current);
-    }
-  });
-
-  return uniq;
-};
-
 const groupBy = (array, func) => {
-  const sortedArr = array.sort();
-  const uniqArr = getUniqArr(sortedArr, func);
-  const obj = {};
+  return array.reduce((obj, item) => {
+    const key = func(item);
 
-  uniqArr.forEach((item, idx) => {
-    const value = sortedArr.filter((el) => func(el) === item);
-    obj[item] = value;
-  });
+    if (!obj.hasOwnProperty(key)) {
+      obj[key] = [];
+    }
 
-  return obj;
+    obj[key].push(item);
+
+    return obj;
+  }, {});
 };
 
 console.log(groupBy([3.6, 3.7, 6.4, 8.9], Math.floor)); // { '3': [3.6, 3.7], '6': [6.4], '8': [8.9] }
