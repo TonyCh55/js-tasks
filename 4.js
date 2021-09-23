7abdea0b5390c783ba19bfb7a709f1fa1574da38
TonyCh55 <chuprunant@gmail.com>
2021-09-23 13:40:05 +0300

    js4 changes

--- 4.js
+++ 4.js
@@ -57,34 +57,106 @@ const database = {
 };
 
 ////////////////////////////////////////////////////////////
-const buyBookForUser = async (bookId, userId, callback) => {
-  try {
-    const user = await database.getUser(userId, (err, user) => {
-      if (err) {
-        callback(err);
-      }
-      return user;
-    });
 
-    const userBooks = await database.getUsersBook(userId, (err, userBooks) => {
+const buyBookForUser = (bookId, userId, callback) => {
+  const promise = new Promise((res, rej) => {
+    database.getUser(userId, (err, user) => {
       if (err) {
-        callback(err);
-      } else if (userBooks.includes(bookId)) {
-        callback(`User already has book with id=${bookId}`);
-      } else {
-        const buyBook = database.buyBook(bookId, (err) => {
-          if (err) {
-            callback(err);
-          } else {
-            callback(null, "Success");
-          }
-        });
+        rej(err);
       }
+      res(user);
     });
-  } catch (err) {
-    throw new Error(err.message);
-  }
+  })
+    .then((user) =>
+      database.getUsersBook(user.id, (err, userBooks) => {
+        if (userBooks && userBooks.includes(bookId)) {
+          callback(`User already has book with id=${bookId}`);
+        }
+      })
+    )
+    .then(() =>
+      database.buyBook(bookId, (err) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, "Success");
+        }
+      })
+    )
+    .catch((err) => callback(err));
+
+  return promise;
 };
+
+// const buyBookForUser = (bookId, userId, callback) => {
+//   const newObj = {
+//     getUser: {
+//       resolve: undefined,
+//       reject: undefined,
+//     },
+//     getUsersBook: {
+//       resolve: undefined,
+//       reject: undefined,
+//     },
+
+//     buyBook: {
+//       resolve: undefined,
+//       reject: undefined,
+//     },
+//   };
+
+//   const someFn = (type) => (reject, resolve) => {
+//     if (reject) {
+//       console.log(reject);
+//     }
+//     newObj[type] = { reject, resolve };
+//   };
+
+//   database.getUser(userId, someFn("getUser"));
+//   database.getUsersBook(userId, someFn("getUsersBook"));
+
+//   if (
+//     newObj.getUsersBook.resolve &&
+//     newObj.getUsersBook.resolve.includes(bookId)
+//   ) {
+//     database.buyBook(bookId, someFn("buyBook"));
+//     callback(`User already has book with id=${bookId}`);
+//   }
+
+//   if (newObj.buyBook.reject) {
+//     callback(true);
+//   } else {
+//     callback(null, "Success");
+//   }
+// };
+//////////////////////////////////////////////////////////
+
+// try {
+//   const user = await database.getUser(userId, (err, user) => {
+//     if (err) {
+//       callback(err);
+//     }
+//     return user;
+//   });
+
+//   const userBooks = await database.getUsersBook(userId, (err, userBooks) => {
+//     if (err) {
+//       callback(err);
+//     } else if (userBooks.includes(bookId)) {
+//       callback(`User already has book with id=${bookId}`);
+//     } else {
+//       const buyBook = database.buyBook(bookId, (err) => {
+//         if (err) {
+//           callback(err);
+//         } else {
+//           callback(null, "Success");
+//         }
+//       });
+//     }
+//   });
+// } catch (err) {
+//   throw new Error(err.message);
+// }
 
 const test = async () => {
   await buyBookForUser(1, 1, (err, message) => {
